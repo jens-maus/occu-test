b0229f4989609dce16eb9ecf0d23b1eea37164fa
hobbyquaker <bastiraff@googlemail.com>
2017-10-29 21:08:27 +0100

    sudo

--- install_rega.sh
+++ install_rega.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 echo "creating directories"
-mkdir /etc/config
-mkdir -p /www/rega
-mkdir /config
+sudo mkdir /etc/config
+sudo mkdir -p /www/rega
+sudo mkdir /config
 
 echo "creating hook scripts"
-echo -e "#!/bin/bash\necho /bin/hm_startup executed" > /bin/hm_startup
-echo -e "#!/bin/bash\necho /bin/hm_autoconf executed" > /bin/hm_autoconf
-chmod a+x /bin/hm_startup /bin/hm_autoconf
+sudo echo -e "#!/bin/bash\necho /bin/hm_startup executed" > /bin/hm_startup
+sudo echo -e "#!/bin/bash\necho /bin/hm_autoconf executed" > /bin/hm_autoconf
+sudo chmod a+x /bin/hm_startup /bin/hm_autoconf
 
 echo "cloning repository"
-git clone https://github.com/eq-3/occu /occu
+sudo git clone https://github.com/eq-3/occu /occu
 
 echo "copying files"
-cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/rega.conf /etc/
-cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config/InterfacesList.xml /etc/config/
-cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/bin/* /bin/
+sudo cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/rega.conf /etc/
+sudo cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config/InterfacesList.xml /etc/config/
+sudo cp -v /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/bin/* /bin/
 
 echo "installing libs"
-apt-get install libc6:i386 libstdc++6:i386
+sudo apt-get install libc6:i386 libstdc++6:i386
+echo "adding /occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/lib/ to ldconfig"
 echo "/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/lib/" > /etc/ld.so.conf.d/hm.conf
-ldconfig
+sudo ldconfig
