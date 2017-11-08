0b9e1116f7cbc0909b467e93e63c1b8ceda2dadf
hobbyquaker <bastiraff@googlemail.com>
2017-11-08 20:45:40 +0100

    faketime tests still not working...:(

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -11,62 +11,54 @@ let {
     regaBuffer
 } = require('../lib/helper.js');
 
-['', '.normal', '.community'].forEach(flavor => {
-
-    describe('rfd/hmipserver Simulator', () => {
-        it('should start', function () {
-            startSim();
-        });
-    });
+['' /*, '.normal', '.community'*/].forEach(flavor => {
 
 
-    describe('start ReGaHss' + flavor + ' faketime tests 2020-03-29 00:59:30 (leap year, begin of DST)', () => {
-        it('should start ReGaHss' + flavor, () => {
-            startRega(flavor, '2020-03-29 00:59:30');
-        });
-        it('should wait 15 seconds', function (done) {
-            this.timeout(16000);
-            setTimeout(done, 15000);
-        });
-
-    });
-
-    describe('timer tests', () => {
-        it('should PRESS_LONG BidCoS-RF:11 at 01:00 (program Timer0100)', function (done) {
-            this.timeout(60000);
-            subscribe('sim', /BidCoS-RF:11/, () => {
-                done();
+    function test(time, program, id) {
+        describe('start ReGaHss' + flavor + ' faketime tests ' + time, () => {
+            it('should start ReGaHss' + flavor, () => {
+                startRega(flavor, time);
             });
         });
-    });
 
-
-    describe('stop ReGaHss' + flavor + ' process', () => {
-        it('should wait 5 seconds', function (done) {
-            this.timeout(6000);
-            setTimeout(done, 5000);
-        });
-
-        it('should stop', function (done) {
-            this.timeout(60000);
-            procs.rega.on('close', () => {
-                procs.rega = null;
-                done();
+        describe('timer tests', () => {
+            it('should call Program ID = ' + id + ' (program ' + program + ')', function (done) {
+                this.timeout(90000);
+                subscribe('rega', new RegExp('execute Program ID = ' + id), output => {
+                    console.log(output);
+                    done();
+                });
             });
-            cp.spawnSync('killall', ['-s', 'SIGINT', 'ReGaHss' + flavor]);
         });
 
-        it('should wait 5 seconds', function (done) {
-            this.timeout(6000);
-            setTimeout(done, 5000);
+
+        describe('stop ReGaHss' + flavor + ' process', () => {
+            it('should stop', function (done) {
+                this.timeout(60000);
+                procs.rega.on('close', () => {
+                    procs.rega = null;
+                    done();
+                });
+                cp.spawnSync('killall', ['-s', 'SIGINT', 'ReGaHss' + flavor]);
+            });
+
         });
-    });
+    }
 
 
-    describe('stop simulator', () => {
-        it('should stop', function () {
-            procs.sim.kill();
-        });
-    });
+  /*
+
+    FIXME tests don't work :( Tests shitty or Rega Bug?
+
+    test('2020-03-28 23:59:40', 'Time0100', '1314');
+    test('2020-03-28 00:29:40', 'Time0130', '1430');
+    test('2020-03-28 00:54:40', 'Time0155', '1458');
+    test('2020-03-29 00:59:40', 'Time0200', '1470');
+    test('2020-03-29 01:04:40', 'Time0205', '1478');
+    test('2020-03-29 01:29:40', 'Time0230', '1510');
+    test('2020-03-29 01:54:40', 'Time0255', '1522');
+    test('2020-03-29 01:59:40', 'Time0300', '1534');
+    test('2020-03-29 02:04:40', 'Time0305', '1546');
+    test('2020-03-29 02:29:40', 'Time0330', '1558');*/
 
 });
