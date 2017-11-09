47aa30cdc898ea6de29809199c56612109ccd1a2
hobbyquaker <bastiraff@googlemail.com>
2017-11-09 21:22:46 +0100

    inherit stdio for date

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -17,7 +17,7 @@ let {
     function test(time, program, id) {
         describe('start ReGaHss' + flavor + ' faketime tests ' + time, () => {
             it('should start ReGaHss' + flavor, () => {
-                cp.spawnSync('/bin/date', [time], {stdio: inherit});
+                cp.spawnSync('/bin/date', [time], {stdio: 'inherit'});
                 startRega(flavor);
             });
         });
