038b3974f6b85ec6542aa19d906a86eb0638fade
hobbyquaker <bastiraff@googlemail.com>
2017-11-09 20:55:36 +0100

    test setting date

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -11,19 +11,20 @@ let {
     regaBuffer
 } = require('../lib/helper.js');
 
-['', '.normal', '.community'].forEach(flavor => {
+[''/*, '.normal', '.community'*/].forEach(flavor => {
 
 
     function test(time, program, id) {
         describe('start ReGaHss' + flavor + ' faketime tests ' + time, () => {
             it('should start ReGaHss' + flavor, () => {
-                startRega(flavor, time);
+                cp.spawnSync('/bin/date', [time]);
+                startRega(flavor);
             });
         });
 
         describe('timer tests', () => {
             it('should call Program ID = ' + id + ' (program ' + program + ')', function (done) {
-                this.timeout(90000);
+                this.timeout(150000);
                 subscribe('rega', new RegExp('execute Program ID = ' + id), output => {
                     console.log(output);
                     done();
@@ -46,19 +47,15 @@ let {
     }
 
 
-
-
-  //  FIXME tests don't work :( Tests shitty or Rega Bug?
-
-    test('2020-03-28 23:59:40', 'Time0100', '1314');
-    test('2020-03-28 00:29:40', 'Time0130', '1430');
-    test('2020-03-28 00:54:40', 'Time0155', '1458');
-    test('2020-03-29 00:59:40', 'Time0200', '1470');
-    test('2020-03-29 01:04:40', 'Time0205', '1478');
-    test('2020-03-29 01:29:40', 'Time0230', '1510');
-    test('2020-03-29 01:54:40', 'Time0255', '1522');
-    test('2020-03-29 01:59:40', 'Time0300', '1534');
-    test('2020-03-29 02:04:40', 'Time0305', '1546');
-    test('2020-03-29 02:29:40', 'Time0330', '1558');
+    test('032823592020.40', 'Time0100', '1314');
+    test('032800292020.40', 'Time0130', '1430');
+    test('032800542020.40', 'Time0155', '1458');
+    test('032900592020.40', 'Time0200', '1470');
+    test('032901042020.40', 'Time0205', '1478');
+    test('032901292020.40', 'Time0230', '1510');
+    test('032901542020.40', 'Time0255', '1522');
+    test('032901592020.40', 'Time0300', '1534');
+    test('032902042020.40', 'Time0305', '1546');
+    test('032902292020.40', 'Time0330', '1558');
 
 });
