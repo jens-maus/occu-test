e3c039386750003d526b56e7545ee0639781ccca
hobbyquaker <bastiraff@googlemail.com>
2017-11-14 21:31:17 +0100

    still tinkering with fake time...

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -15,13 +15,15 @@ let {
 
     function test(time, program, id) {
         describe('fake time test ' + time, function () {
-            it('should start ReGaHss' + flavor, function () {
+            it('should start ReGaHss' + flavor, function (done) {
+                this.timeout(30000);
                 console.log('    sudo /bin/date ' + time);
                 cp.exec('sudo /bin/date ' + time, () => {
                     cp.exec('/bin/date', (err, stdout, stderr) => {
                         console.log('    ' + stdout.replace('\n', ''));
                         startRega(flavor);
                     });
+                    done();
                 });
             });
         });
