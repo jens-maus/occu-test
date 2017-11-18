dedf8c50c0ad233ec9cec75c4ee07a477d611909
hobbyquaker <bastiraff@googlemail.com>
2017-11-18 12:45:23 +0100

    prepare error handling tests

---
+++ test/12-rpc-errors.js
@@ -0,0 +1,22 @@
+let {
+    cp,
+    rega,
+    subscribe,
+    startRega,
+    startSim,
+    procs,
+    simSubscriptions,
+    simBuffer,
+    regaSubscriptions,
+    regaBuffer,
+    flavors
+} = require('../lib/helper.js');
+
+flavors.forEach(flavor => {
+
+    describe('test script error handling', () => {
+
+
+    });
+
+});
