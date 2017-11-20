0ae9713066524e366936594f93c0bb2787e5251a
Jens Maus <mail@jens-maus.de>
2017-11-20 17:23:49 +0100

    updated test layout of 08-program-trigger-variable.js

--- test/08-program-trigger-variable.js
+++ test/08-program-trigger-variable.js
@@ -16,82 +16,63 @@ require('should');
 
 flavors.forEach(flavor => {
 
-    describe('rfd/hmipserver Simulator', () => {
-        it('should start', function () {
-            startSim();
-        });
-    });
+    describe('Running ' + __filename.split('/').reverse()[0] + ' test...', () => {
 
-    describe('ReGaHss' + flavor, () => {
-
-        it('should start ReGaHss' + flavor, () => {
-            startRega(flavor);
-        });
-        it('should start HTTP server', function (done) {
-            this.timeout(30000);
-            subscribe('rega', /HTTP server started successfully/, () => {
-                done();
+        describe('rfd/hmipserver Simulator', () => {
+            it('should start', function () {
+                startSim();
             });
         });
-        it('should wait 10 seconds', function (done) {
-            this.timeout(11000);
-            setTimeout(done, 10000);
-        });
-    });
 
-
-
-    describe('variable change triggers program', () => {
-
-        it('should PRESS_LONG BidCoS-RF:12 when VarBool1 changes to true (program Bool1OnTrue)', function (done) {
-            this.timeout(7000);
-            subscribe('sim', /setValue rfd BidCoS-RF:12 PRESS_LONG true/, () => {
-                done();
+        describe('starting ReGaHss' + flavor, () => {
+            it('should start', () => {
+                startRega(flavor);
             });
-            rega.exec('var b1 = dom.GetObject(1237);\nb1.State(true);');
-        });
-        it('should PRESS_LONG BidCoS-RF:13 when VarBool1 changes to false (program Bool1OnTrue)', function (done) {
-            this.timeout(7000);
-            subscribe('sim', /setValue rfd BidCoS-RF:13 PRESS_LONG true/, () => {
-                done();
+            it('wait for HTTP server to be ready', function (done) {
+                this.timeout(60000);
+                subscribe('rega', /HTTP server started successfully/, () => {
+                    done();
+                });
             });
-            rega.exec('var b1 = dom.GetObject(1237);\nb1.State(false);');
-        });
-    });
-
-
-
-
-    describe('stop ReGaHss' + flavor + ' process', () => {
-        /*
-        it('should wait 5 seconds', function (done) {
-            this.timeout(6000);
-            setTimeout(done, 5000);
-        });
-        */
-
-        it('should stop', function (done) {
-            this.timeout(60000);
-            procs.rega.on('close', () => {
-                procs.rega = null;
-                done();
+            it('should wait 10 seconds', function (done) {
+                this.timeout(11000);
+                setTimeout(done, 10000);
             });
-            cp.spawnSync('killall', ['-s', 'SIGINT', 'ReGaHss' + flavor]);
         });
 
-        /*
-        it('should wait 5 seconds', function (done) {
-            this.timeout(6000);
-            setTimeout(done, 5000);
+        describe('variable change triggers program', () => {
+
+            it('should PRESS_LONG BidCoS-RF:12 when VarBool1 changes to true (program Bool1OnTrue)', function (done) {
+                this.timeout(7000);
+                subscribe('sim', /setValue rfd BidCoS-RF:12 PRESS_LONG true/, () => {
+                    done();
+                });
+                rega.exec('var b1 = dom.GetObject(1237);\nb1.State(true);');
+            });
+            it('should PRESS_LONG BidCoS-RF:13 when VarBool1 changes to false (program Bool1OnTrue)', function (done) {
+                this.timeout(7000);
+                subscribe('sim', /setValue rfd BidCoS-RF:13 PRESS_LONG true/, () => {
+                    done();
+                });
+                rega.exec('var b1 = dom.GetObject(1237);\nb1.State(false);');
+            });
         });
-        */
+
+        describe('stopping ReGaHss' + flavor, () => {
+            it('should stop', function (done) {
+                this.timeout(60000);
+                procs.rega.on('close', () => {
+                    procs.rega = null;
+                    done();
+                });
+                cp.spawnSync('killall', ['-9', 'ReGaHss' + flavor]);
+            });
+        });
+
+        describe('stop simulator', () => {
+            it('should stop', function () {
+                procs.sim.kill();
+           });
+        });
     });
-
-
-    describe('stop simulator', () => {
-        it('should stop', function () {
-            procs.sim.kill();
-        });
-    });
-
 });
