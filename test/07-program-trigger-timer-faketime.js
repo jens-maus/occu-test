821fdf553b7987f50168433528be6408cc184c00
Jens Maus <mail@jens-maus.de>
2018-10-27 11:23:29 +0200

    enable period timer test again with some slightly better test values.

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -55,11 +55,9 @@ flavors.forEach(function (flavor) {
         // Perform long running timer test for normal datetime
         test('2019-12-31 23:58:48 CET', 'TimeEveryMinute', '1302', 3, 65000);
 
-        // Perform long running timer test at DST boundaries
-        /* FIXME: produces errors in all ReGa variants!
-        test('2020-10-25 02:58:40 CEST', 'TimeEveryMinute', '1302', 3, 65000);
-        test('2020-03-29 01:58:40 CET', 'TimeEveryMinute', '1302', 3, 65000);
-        */
+        // Perform long running regular timer test at DST boundaries
+        test('2020-10-25 02:58:40 CEST', 'TimeEveryMinute', '1302', 5, 300000);
+        test('2020-03-29 01:58:40 CET', 'TimeEveryMinute', '1302', 5, 300000);
 
         // Leap year, Feb, 29. 2020
         test('2020-02-29 01:59:48 CET', 'Time0200', '1470');
