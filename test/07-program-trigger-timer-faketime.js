4a5195ff549f0b813b296f3b52fcb410a1783a1c
Jens Maus <mail@jens-maus.de>
2019-03-13 16:36:35 +0100

    add test to check if ReGaHss is still running after a test.

--- test/07-program-trigger-timer-faketime.js
+++ test/07-program-trigger-timer-faketime.js
@@ -50,31 +50,35 @@ flavors.forEach(function (flavor) {
 
     describe('Running ' + __filename.split('/').reverse()[0] + ' [' + flavor + ']', function () {
         // Perform normal timer test for single execution
-        test('2020-01-01 00:59:48 CET', 'Time0100', '1314');
+        test('2020-01-01 00:59:48 CET',  'Time0100', '1314');
 
         // Perform long running timer test for normal datetime
-        test('2019-12-31 23:58:48 CET', 'TimeEveryMinute', '1302', 3, 65000);
+        test('2019-12-31 23:58:48 CET',  'TimeEveryMinute', '1302', 3, 65000);
 
         // Perform long running timer test for DST boundaries
         // (1 day after/before DST switch)
-        test('2019-03-31 23:58:48 CET', 'TimeEveryMinute', '1302', 3, 65000);
-        test('2019-03-31 03:29:48 CEST','Time0330', '1558');
-        test('2019-04-01 23:58:48 CEST','TimeEveryMinute', '1302', 3, 65000);
-        test('2019-04-01 01:59:48 CEST','Time0200', '1470');
-        test('2019-04-01 02:04:48 CEST','Time0205', '1498');
+        test('2019-03-30 03:04:48 CET',  'Time0305', '1546');
+        test('2019-03-30 03:29:48 CET',  'Time0330', '1558');
+        test('2019-03-30 23:58:48 CET',  'TimeEveryMinute', '1302', 5, 70000);
+        test('2019-03-31 03:04:48 CEST', 'Time0305', '1546');
+        test('2019-03-31 03:29:48 CEST', 'Time0330', '1558');
+        test('2019-03-31 23:58:48 CEST', 'TimeEveryMinute', '1302', 5, 70000);
+        test('2019-04-01 01:59:48 CEST', 'Time0200', '1470');
+        test('2019-04-01 02:04:48 CEST', 'Time0205', '1498');
+        test('2019-04-01 23:58:48 CEST', 'TimeEveryMinute', '1302', 5, 70000);
 
         // Perform long running regular timer test at DST boundaries
         test('2020-10-25 02:58:40 CEST', 'TimeEveryMinute', '1302', 5, 70000);
-        test('2020-03-29 01:58:40 CET', 'TimeEveryMinute', '1302', 5, 70000);
+        test('2020-03-29 01:58:40 CET',  'TimeEveryMinute', '1302', 5, 70000);
 
         // Leap year, Feb, 29. 2020
-        test('2020-02-29 01:59:48 CET', 'Time0200', '1470');
+        test('2020-02-29 01:59:48 CET',  'Time0200', '1470');
 
         // -> start of DST (winter->summer) in leap year
-        test('2020-03-29 00:59:48 CET', 'Time0100', '1314');
-        test('2020-03-29 01:29:48 CET', 'Time0130', '1430');
-        test('2020-03-29 01:54:48 CET', 'Time0155', '1458');
-        test('2020-03-29 01:59:48 CET', 'Time0200', '1470');
+        test('2020-03-29 00:59:48 CET',  'Time0100', '1314');
+        test('2020-03-29 01:29:48 CET',  'Time0130', '1430');
+        test('2020-03-29 01:54:48 CET',  'Time0155', '1458');
+        test('2020-03-29 01:59:48 CET',  'Time0200', '1470');
         // test('2020-03-29 02:04:48 CET', 'Time0205', '1498'); // not in DST
         // test('2020-03-29 02:29:48 CET', 'Time0230', '1510'); // not in DST
         // test('2020-03-29 02:54:48 CET', 'Time0255', '1522'); // not in DST
