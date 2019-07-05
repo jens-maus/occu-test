0c6b2799f6f26c863d61939fd3ff85027b21e5c1
Jens Maus <mail@jens-maus.de>
2019-07-05 09:01:15 +0200

    lets get npm running

--- run.sh
+++ run.sh
@@ -2,6 +2,16 @@
 
 FLAVOR=${FLAVOR:-beta}
 
+source ~/.bashrc
+source ~/.profile
+if [ ! -d ${NVM_DIR} ]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+  source ~/.bashrc
+  source ~/.profile
+fi
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 echo "STEP: running occu test..."
 export FLAVOR=${FLAVOR}
 npm test ${1}
