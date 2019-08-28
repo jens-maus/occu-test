547301fe06999c7eb4f5b1a01288506827beb988
Jens Maus <mail@jens-maus.de>
2019-08-28 13:36:29 +0200

    async.whilst() seem to work now, lets remove debug output.

--- install.sh
+++ install.sh
@@ -70,6 +70,4 @@ fi
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 6
-pwd
 npm install
-npm list async
